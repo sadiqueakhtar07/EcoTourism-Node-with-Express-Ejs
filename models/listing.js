bd1714fa1ce85de81c274843545cfd64c809d169
Sadique Akhtar <contacttosadique@gmail.com>
2026-01-11 23:57:10 +0530

    make review section

--- models/listing.js
+++ models/listing.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 
 const listingSchema = new mongoose.Schema({
@@ -31,7 +32,14 @@ const listingSchema = new mongoose.Schema({
     },
     country: {
         type: String,
-    }
+    },
+
+    reviews:[
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Review",
+        }
+    ],
 });
 
 const Listing = mongoose.model('Listing', listingSchema);
